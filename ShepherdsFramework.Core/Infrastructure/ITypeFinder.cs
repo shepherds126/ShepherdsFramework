6f5279920b86c4eed167f9e13011da54a9bbb06e
一桥清雨 <shepherds126@126.com>
2015-08-14 17:55:05 +0800

    Revert "增加TypeFinder程序集类型自动查找"
    This reverts commit 75f435c888e24e0c9f048813e338db41306688a1.

--- ShepherdsFramework.Core/Infrastructure/ITypeFinder.cs
+++ ShepherdsFramework.Core/Infrastructure/ITypeFinder.cs
@@ -7,45 +7,12 @@ using System.Threading.Tasks;
 
 namespace ShepherdsFramework.Core.Infrastructure
 {
-    /// <summary>
-    /// 类型查找抽象接口
-    /// </summary>
     public interface ITypeFinder
     {
         /// <summary>
-        /// 获取程序集
+        /// 获取所有的程序集
         /// </summary>
         /// <returns></returns>
         IList<Assembly> GetAssemblies();
-        /// <summary>
-        /// 查找满足指定类型的的所有类型的集合
-        /// </summary>
-        /// <param name="assignTypeFrom">制定类型</param>
-        /// <param name="onlyConcreteClasses">默认为实体类</param>
-        /// <returns></returns>
-        IEnumerable<Type> FindClassesOfType(Type assignTypeFrom, bool onlyConcreteClasses = true);
-        /// <summary>
-        /// 查找满足指定类型的的所有类型的集合
-        /// </summary>
-        /// <param name="assignTypeFrom"></param>
-        /// <param name="assemblies"></param>
-        /// <param name="onlyConcreteClasses"></param>
-        /// <returns></returns>
-        IEnumerable<Type> FindClassesOfType(Type assignTypeFrom, IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true);
-        /// <summary>
-        /// 查找满足指定类型的的所有类型的集合
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="onlyConcreteClasses"></param>
-        /// <returns></returns>
-        IEnumerable<Type> FindClassesOfType<T>(bool onlyConcreteClasses = true);
-        /// <summary>
-        /// 查找满足指定类型的的所有类型的集合
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="assemblies"></param>
-        /// <param name="onlyConcreteClasses"></param>
-        /// <returns></returns>
-        IEnumerable<Type> FindClassesOfType<T>(IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true);
     }
 }
