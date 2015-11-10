98bd50ce6823ad0acd18d3a15f064073d81e6c74
一桥清雨 <shepherds126@126.com>
2015-11-10 10:27:49 +0800

    增加日志适配器

---
+++ ShepherdsFramework.Core/Logging/ILoggerFactoryAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShepherdsFramework.Core.Logging
+{
+    /// <summary>
+    /// 供LoggerFactory使用的适配器接口
+    /// </summary>
+    public interface ILoggerFactoryAdapter
+    {
+        /// <summary>
+        /// 依据日志名称获取
+        /// </summary>
+        /// <param name="loggerName">日志名称</param>
+        /// <returns></returns>
+         ILogger GetLogger(string loggerName);
+    }
+}
